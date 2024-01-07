ed32e83bb74f5cdfbde37785449d98477e8508a4
Arsenty Halimovsky <ghost12@yandex.ru>
2024-01-07 15:09:15 +0300

    Create a new project

---
+++ sber21GrepCustom/sber21GrepCustom/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  sber21GrepCustom
+//
+//  Created by Арсентий Халимовский on 07.01.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
